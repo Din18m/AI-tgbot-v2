e2f1a723d47d52612e3b85fa200d0aa819c60d51
PdrsDv3 <denis.kalinkin.05@gmail.com>
2025-01-30 22:58:59 +0300

    nth

--- general_notices.py
+++ general_notices.py
@@ -1,15 +1,21 @@
 from config import bot
 
 
-async def notify_before_interview(id_teacher, id_student, window):
+async def notify_before_interview(id_teacher, nick_tch, id_student, nick_st, window):
     datetime_obj = window['time']
     date = datetime_obj.strftime("%d.%m")
     time = datetime_obj.strftime("%H:%M")
     description = window['description']
     await bot.send_message(chat_id=id_teacher, text=)
-    await bot.send_message(chat_id=id_student, text="")
+    await bot.send_message(chat_id=id_student, text=)
 
 
-NOTIFY_TEXT_for_student="""
+TEXT_for_student="""
+Напоминание.
+Вы записаны на собеседование
+{}
+"""
+
+TEXT_for_teacher="""
 
 """
